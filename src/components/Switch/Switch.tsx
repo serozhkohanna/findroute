21a1d213fbfc5e1789417e5cb74d5cda434c3680
serozhkohanna <serozhkohanna@gmail.com>
2021-01-13 18:59:02 +0200

    Created switch component.

---
+++ src/components/Switch/Switch.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Switch.scss';
+
+const Switch = () => {
+  return <div>
+	swithc
+  </div>
+}
+
+export default Switch;
--- src/pages/StepsPage/StepsPage.tsx
+++ src/pages/StepsPage/StepsPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './StepsPage.scss';
+import Switch from "../../components/Switch/Switch";
 
 const StepsPage = () => {
-  return <main>
-	steps here
+  return <main className='page-steps'>
+	<Switch/>
   </main>
-}
+};
 
 export default StepsPage;
