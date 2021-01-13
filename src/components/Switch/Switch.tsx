0ee996955cab3c54365a72488dd2492b965c2495
serozhkohanna <serozhkohanna@gmail.com>
2021-01-13 20:21:22 +0200

    Form in origin loction.

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './Switch.scss';
+import { steps } from "../../constants/routes";
 
-const Switch = () => {
-  return <div>
-	swithc
-  </div>
+interface Props {
+  activeStep: string;
+  setSwitch: Function;
+}
+
+const Switch: FC<Props> = ({activeStep, setSwitch}) => {
+  const handleTabSwitch = (step: string) => {
+	setSwitch(step);
+  }
+
+  return <nav className='switch'>
+	<a className={`${activeStep === steps.origin && 'is-active'} tab-item`} href='#origin'
+	   onClick={() => handleTabSwitch(steps.origin)}>Origin</a>
+	<a className={`${activeStep === steps.location && 'is-active'} tab-item`} href='#location'
+	   onClick={() => handleTabSwitch(steps.location)}>Destination</a>
+  </nav>
 }
 
 export default Switch;
--- src/pages/StepsPage/StepsPage.tsx
+++ src/pages/StepsPage/StepsPage.tsx
@@ -4,34 +4,39 @@ import Switch from "../../components/Switch/Switch";
 import { steps } from "../../constants/routes";
 
 const StepsPage = () => {
-  const [isActiveTab, setActiveTab] = useState(steps.origin);
+  const [activeTab, setActiveTab] = useState(steps.origin);
+  const [originLocation, setOriginLocation] = useState();
 
   const handleTabSwitch = (tab: string) => {
 	setActiveTab(tab);
   }
 
-  const renderTabContent = () => {
-	return <div>
+  const handleOriginSubmit = () => {
 
-	</div>
+  }
+
+  const handleOriginLocation = (e: any) => {
+	setOriginLocation(e.target.value);
   }
 
   return <main className='page-steps'>
-	{/*<Switch/>*/}
-	<nav className="modal-tabs-names">
-	  <a className={`${isActiveTab === steps.origin && 'is-active'} tab-item`} href='#origin'
-		 onClick={() => handleTabSwitch(steps.origin)}>All content</a>
-	  <a className={`${isActiveTab === steps.location && 'is-active'} tab-item`} href='#location'
-		 onClick={() => handleTabSwitch(steps.location)}>Books</a>
-	</nav>
-
+	<Switch setSwitch={handleTabSwitch} activeStep={activeTab}/>
 	<div className="steps-form">
-	  <div id="origin" className={`${isActiveTab === steps.origin && 'is-active'} form-content`}>
-		first
+	  <div id="origin" className={`${activeTab === steps.origin && 'is-active'} step-content`}>
+		<h2>set destination</h2>
+		<form className='form' onSubmit={handleOriginSubmit}>
+		  <input value={originLocation} onChange={handleOriginLocation} type="text" placeholder='Set initial location'/>
+		  <button disabled={!originLocation}>
+			NEXT
+		  </button>
+		</form>
 	  </div>
-	  <div id="location" className={`${isActiveTab === steps.location && 'is-active'} form-content`}>
-		second
+	  <div id="location" className={`${activeTab === steps.location && 'is-active'} step-content`}>
+		location info
 	  </div>
+	</div>
+	<div className="label">
+	  On next steps you could extend your search by setting intermediate locations and path preferances
 	</div>
 
   </main>
