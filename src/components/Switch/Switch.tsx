77435bb2a0dacbf6aee03df9f5be71846b7ede62
serozhkohanna <serozhkohanna@gmail.com>
2021-01-20 17:00:19 +0200

    set routing.

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -5,18 +5,23 @@ import { steps } from "../../constants/routes";
 interface Props {
   activeStep: string;
   setSwitch: Function;
+  filledStep: number;
 }
 
-const Switch: FC<Props> = ({activeStep, setSwitch}) => {
+const Switch: FC<Props> = ({activeStep, setSwitch, filledStep}) => {
   const handleTabSwitch = (step: string) => {
 	setSwitch(step);
   }
 
   return <nav className='switch'>
-	<a className={`${activeStep === steps.origin && 'is-active'} tab-item`} href='#origin'
-	   onClick={() => handleTabSwitch(steps.origin)}>Origin</a>
-	<a className={`${activeStep === steps.location && 'is-active'} tab-item`} href='#location'
-	   onClick={() => handleTabSwitch(steps.location)}>Destination</a>
+	<button className={`${activeStep === steps.origin && 'is-active'} tab-item`}
+			onClick={() => handleTabSwitch(steps.origin)}>
+	  <a href='#origin'>Origin</a>
+	</button>
+	<button disabled={filledStep < 1} className={`${activeStep === steps.location && 'is-active'} tab-item`}
+			onClick={() => handleTabSwitch(steps.location)}>
+	  <a href='#location'>Destination</a>
+	</button>
   </nav>
 }
 
--- src/pages/StepsPage/StepsPage.tsx
+++ src/pages/StepsPage/StepsPage.tsx
@@ -1,38 +1,60 @@
 import React, { useState } from 'react';
 import './StepsPage.scss';
+import { useHistory } from 'react-router'
+
 import Switch from "../../components/Switch/Switch";
-import { steps } from "../../constants/routes";
+import { steps, INFO_PAGE } from "../../constants/routes";
 
 const StepsPage = () => {
   const [activeTab, setActiveTab] = useState(steps.origin);
   const [originLocation, setOriginLocation] = useState();
+  const [destinationLocation, setDestinationLocation] = useState();
+  const [filledStep, setFilledStep] = useState(0);
+
+  const history = useHistory();
 
   const handleTabSwitch = (tab: string) => {
 	setActiveTab(tab);
   }
 
-  const handleOriginSubmit = () => {
+  const handleOriginSubmit = (e: any) => {
+    e.preventDefault();
+	setFilledStep(1);
+  }
 
+  const handleDestinationSubmit = (e: any) => {
+	e.preventDefault();
+	history.push(INFO_PAGE);
   }
 
   const handleOriginLocation = (e: any) => {
 	setOriginLocation(e.target.value);
   }
 
+  const handleDestinationLocation = (e: any) => {
+	setDestinationLocation(e.target.value);
+  }
+
   return <main className='page-steps'>
-	<Switch setSwitch={handleTabSwitch} activeStep={activeTab}/>
+	<Switch setSwitch={handleTabSwitch} activeStep={activeTab} filledStep={filledStep}/>
 	<div className="steps-form">
 	  <div id="origin" className={`${activeTab === steps.origin && 'is-active'} step-content`}>
-		<h2>set destination</h2>
+		<h2>set origin</h2>
 		<form className='form' onSubmit={handleOriginSubmit}>
 		  <input value={originLocation} onChange={handleOriginLocation} type="text" placeholder='Set initial location'/>
-		  <button disabled={!originLocation}>
-			NEXT
+		  <button disabled={!originLocation} onClick={() => handleTabSwitch(steps.location)}>
+			next
 		  </button>
 		</form>
 	  </div>
 	  <div id="location" className={`${activeTab === steps.location && 'is-active'} step-content`}>
-		location info
+		<h2>set destination</h2>
+		<form onSubmit={handleDestinationSubmit}>
+		  <input value={destinationLocation} onChange={handleDestinationLocation} type="text" placeholder='Set initial location'/>
+		  <button disabled={!destinationLocation} onClick={() => handleTabSwitch(steps.location)}>
+			next
+		  </button>
+		</form>
 	  </div>
 	</div>
 	<div className="label">
